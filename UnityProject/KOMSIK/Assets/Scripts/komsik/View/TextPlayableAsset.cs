abaed2efc803bbaf86b7efd512ffb69fc386897c
kazushi-sakashita <kazusi@hotmail.co.jp>
2022-05-13 02:23:34 +0900

    [add] タイムラインことはじめ

---
+++ UnityProject/KOMSIK/Assets/Scripts/komsik/View/TextPlayableAsset.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.Playables;
+
+[System.Serializable]
+public class TextPlayableAsset : PlayableAsset
+{
+    [SerializeField,TextArea(1, 6)] private string text;
+    public Color startColor = Color.white;
+    public Color endColor = Color.white;
+    public ExposedReference<GameObject> charaObj;
+    // public string text;
+
+    // Factory method that generates a playable based on this asset
+    public override Playable CreatePlayable(PlayableGraph graph, GameObject go)
+    {
+        var behaviour = new TextPlayableBehaviour();
+        behaviour.charaObject = charaObj.Resolve(graph.GetResolver());
+        behaviour.Text = text;
+        behaviour.StartColor = startColor;
+        behaviour.EndColor = endColor;
+
+        // behaviour.text = text;
+        return ScriptPlayable<TextPlayableBehaviour>.Create(graph, behaviour);
+    }
+}
---
+++ UnityProject/KOMSIK/Assets/Scripts/komsik/View/TextPlayableBehaviour.cs
@@ -0,0 +1,51 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.Playables;
+
+// A behaviour that is attached to a playable
+public class TextPlayableBehaviour : PlayableBehaviour
+{
+    public GameObject charaObject;
+    public string Text { get; set; }
+    public Color StartColor { get; set; }
+    public Color EndColor { get; set; }
+
+    // Called when the owning graph starts playing
+    public override void OnGraphStart(Playable playable)
+    {
+        this.charaObject.GetComponent<TMP_Text>().text = "";
+    }
+
+    // Called when the owning graph stops playing
+    public override void OnGraphStop(Playable playable)
+    {
+        this.charaObject.GetComponent<TMP_Text>().text = this.Text;
+    }
+
+    // Called when the state of the playable is set to Play
+    public override void OnBehaviourPlay(Playable playable, FrameData info)
+    {
+
+    }
+
+    // Called when the state of the playable is set to Paused
+    public override void OnBehaviourPause(Playable playable, FrameData info)
+    {
+
+    }
+
+    // Called each frame while the state is set to Play
+    public override void ProcessFrame(Playable playable, FrameData info, object playerData)
+    {
+        // PlayableTrackのClip上でシークバーが移動するたびに呼ばれ続ける（PrepareFrameの後）
+        if (charaObject == null) { return; }
+        var percent = (float)playable.GetTime() / (float)playable.GetDuration();
+
+        this.charaObject.GetComponent<TMP_Text>().text =
+            this.Text.Substring(0, (int)Mathf.Round(this.Text.Length * percent));
+
+        var progress = (float)(playable.GetTime() / playable.GetDuration());
+        this.charaObject.GetComponent<TMP_Text>().color = Color.Lerp(StartColor, EndColor, progress);
+
+    }
+}
